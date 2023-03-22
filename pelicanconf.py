ccc3c620722ac5941ab90980bb51583bf5c98b40
cp <pchen0825@163.com>
2023-03-22 19:57:58 +0800

    add theme blue-penguin-dark

--- pelicanconf.py
+++ pelicanconf.py
@@ -16,7 +16,10 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
+LINKS = (('Python', 'http://127.0.0.1:8000/category/python.html'),
+         ('Linux', 'http://127.0.0.1:8000/category/linux.html'),
+         ('Tools', 'http://127.0.0.1:8000/category/tools.html'),
+         ('Pelican', 'https://getpelican.com/'),
          ('Python.org', 'https://www.python.org/'),
          ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
          ('You can modify those links in your config file', '#'),)
@@ -28,4 +31,46 @@ SOCIAL = (('You can add links in your config file', '#'),
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# RELATIVE_URLS = True
+
+
+# THEME
+THEME = "themes/blue-penguin-dark"
+
+# HTML metadata
+SITEDESCRIPTION = ''
+
+# all defaults to True.
+DISPLAY_HEADER = True
+DISPLAY_FOOTER = False
+DISPLAY_HOME = True
+DISPLAY_MENU = True
+DARK_LIGHT_SWITCHING_OFF = True
+
+# provided as examples, they make ‘clean’ urls. used by MENU_INTERNAL_PAGES.
+TAGS_URL = 'tags'
+TAGS_SAVE_AS = 'tags/index.html'
+AUTHORS_URL = 'authors'
+AUTHORS_SAVE_AS = 'authors/index.html'
+CATEGORIES_URL = 'categories'
+CATEGORIES_SAVE_AS = 'categories/index.html'
+ARCHIVES_URL = 'archives'
+ARCHIVES_SAVE_AS = 'archives/index.html'
+
+# use those if you want pelican standard pages to appear in your menu
+MENU_INTERNAL_PAGES = (
+    ('Tags', TAGS_URL, TAGS_SAVE_AS),
+    ('Authors', AUTHORS_URL, AUTHORS_SAVE_AS),
+    ('Categories', CATEGORIES_URL, CATEGORIES_SAVE_AS),
+    ('Archives', ARCHIVES_URL, ARCHIVES_SAVE_AS),
+)
+# additional menu items
+MENUITEMS = (
+    ('GitHub', 'https://github.com/'),
+)
+
+# example pagination pattern
+PAGINATION_PATTERNS = (
+    (1, '{url}', '{save_as}'),
+    (2, '{base_name}/page/{number}/', '{base_name}/page/{number}/index.html'),
+)
